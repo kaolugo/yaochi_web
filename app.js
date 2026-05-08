1d138e5865cbc268ea0f915139dc6c71f21397db
kaolugo <kaolugo@umich.edu>
2026-05-08 16:27:54 -0500

    Finished stylizing button bg

--- app.js
+++ app.js
@@ -58,17 +58,64 @@ async function getData() {
 function categorize(data) {
     let result = {};
     data.forEach((item) => {
-        if (result[item.category]) {
-            result[item.category].push(item);
-        } else {
-            result[item.category] = [ item ];
+        const { category, subcategory } = item;
+
+        if (!subcategory) {
+            if (result[category]) {
+                result[category].push(item);
+            } else {
+                result[category] = [ item ];
+            }
+
+            return;
         }
+
+        if (result[category]) {
+            if (result[category][subcategory]) {
+                result[category][subcategory].push(item);
+            } else {
+                result[category][subcategory] = [ item ];
+            }
+        } else {
+            result[category] = {};
+            result[category][subcategory] = [ item ];
+        }
+
+        
     });
     return result;
 }
 
-function renderHTML(category, data) {
+function renderHTML(category, data, subcategorize = false) {
     const container = document.getElementById(category);
+
+    // goal here
+    // render <div class = "menuSubtitle"></div>
+    // at the beginning of the list
+
+    console.log("kaoru", data);
+
+    if (subcategorize) { 
+        let result = ``;
+        for (const [subcategory, menuItems] of Object.entries(data)) {
+            console.log("subcategory", subcategory);
+            console.log("menu items", menuItems);
+            let headHtml = `<div class = "menuSubtitle">${subcategory}</div>`;
+            let html = menuItems.map(item => `
+                <div class = "menuItem">
+                    <div class = "dish">${item.name}</div>
+                    <div class = "price">${item.price}</div>
+                </div>
+            `).join('');
+            let closingHtml = `<div class = "menuCloser"></div>`
+            result = result + headHtml + html + closingHtml;
+        }
+
+        container.innerHTML = result;
+        return;
+    }
+    
+    console.log("kaoru", data);
     container.innerHTML = data.map(item => `
         <div class = "menuItem">
             <div class = "dish">${item.name}</div>
@@ -85,6 +132,11 @@ async function main() {
     
     renderHTML("appetizer", categorizedMenu["とりあえず"]);
     renderHTML("recommendation", categorizedMenu["おすすめ"]);
+    renderHTML("okonomiyaki", categorizedMenu["お好み焼き"], true);
+    renderHTML("yakisoba", categorizedMenu["焼きそば"]);
+    renderHTML("entree", categorizedMenu["一品"]);
+    renderHTML("lunch", categorizedMenu["ランチメニュー"]);
+    renderHTML("drink", categorizedMenu["ドリンク"], true);
 }
 
 main();
